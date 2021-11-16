a2eeb3d887a7c80186d27e9df469bd876074c7f8
jindong <248381089@qq.com>
2021-11-16 18:57:33 +0800

    落地页创建

--- test_case/test_landing_page.py
+++ test_case/test_landing_page.py
@@ -1,4 +1,6 @@
 # coding=utf8
+import random
+import string
 from test_case import *
 
 
@@ -19,7 +21,8 @@ class TestUserLogin(unittest.TestCase):
 
     def test_saveOrder(self):  # 函数名要以test开头，否则不会被执行
         login_data = get_request_data("/api/v1/landing-page/landing-pages/pmp")
-
+        print(login_data)
+        login_data[0]["data"]["name"] = "自动化测试" + ''.join(random.sample(string.digits + string.ascii_letters, 8))
         r = RequestApi()
         r.request(login_data[0])
 
